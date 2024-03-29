ab6577bced3ef9f79fa940148bfdfb03685fff89
Rodrigo Castro <rodrigo@occupier.com>
2024-03-29 17:19:06 -0300

    sd

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { brainwave } from "../assets/";
+import { navigation } from "../constants";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 bg-n-8/900 backdrop:blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 ">
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwave} width={190} height={40} alt="brainwave"></img>
+        </a>
+
+        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row"></div>
+        </nav>
+        {navigation.map((item) => (
+          <a
+            key={item.id}
+            href={item.href}
+            className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover text-color-1`}
+          >
+            {item.title}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
